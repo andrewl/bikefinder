3ba8a4efbdf3bee62eeb5877f2219a7698624f44
Andrew Larcombe <andrew@andrew-mbp.local>
2016-02-02 11:58:00 +0000

    running on openshift

--- main.go
+++ main.go
@@ -30,7 +30,8 @@ func ingest(w http.ResponseWriter, r *http.Request) {
 		IngestionUri string `json:"ingestion_uri"`
 		PublicUri    string `json:"public_uri"`
 	}
-	file, _ := os.Open("./bikefinder.json")
+	fmt.Println("config at ", os.Getenv("BIKEFINDER_CONFIG"))
+	file, _ := os.Open(os.Getenv("BIKEFINDER_CONFIG"))
 	decoder := json.NewDecoder(file)
 	configuration := []Configuration{}
 	err := decoder.Decode(&configuration)
--- readme.txt
+++ readme.txt
@@ -8,3 +8,4 @@ setup
 
 export DATABASE_URL="username:password@tcp(localhost:3306)/dbname"
 export JCDECAUX_API_KEY=
+export BIKEFINDER_CONFIG=<path to json file>
